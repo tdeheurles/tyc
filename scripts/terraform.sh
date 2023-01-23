5affc75709ed98855abe50f09905c390ca9838c7
tdeheurles <tdeheurles@gmail.com>
2023-01-23 12:16:45 +0100

    update githubaction to use same script as for local

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -3,11 +3,13 @@ set -euo pipefail
 
 project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/.."
 
-if [[ ! -e "${project_root}/.aws.secret" ]]; then
-    echo -e "\e[31mError: please create your .aws.secret file (see README.md#define-your-aws-secrets)\e[39m"
-    exit 1
+if [[ "${AWS_ACCESS_KEY_ID+x}" == "" ]]; then
+    if [[ ! -e "${project_root}/.aws.secret" ]]; then
+        echo -e "\e[31mError: please create your .aws.secret file (see README.md#define-your-aws-secrets)\e[39m"
+        exit 1
+    fi
+    source "${project_root}/.aws.secret"
 fi
-source "${project_root}/.aws.secret"
 
 cd ${project_root}/infrastructure/terraform
 
